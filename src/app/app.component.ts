4e30cab5a391b896057d2f2201e4840978a42d84
penninkn <morrikai@oregonstate.edu>
2021-04-08 20:43:40 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projects;
+  backendUrl = 'http://localhost:3000/project/projects';
+  constructor(private http: HttpClient) {}
   title = 'frontend';
+
+  async ngOnInit() {
+    this.projects = await this.http.get<any>(this.backendUrl).toPromise();
+    console.log(this.projects);
+  }
 }
