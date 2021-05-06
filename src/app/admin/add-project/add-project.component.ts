9495b00ce0c7fb2e06366d8956e40e8701dcc9a2
addiegoe <58092893+addiegoe@users.noreply.github.com>
2021-05-06 12:29:42 -0700

    project posts from add-project component and redirects to projects page

--- src/app/admin/add-project/add-project.component.ts
+++ src/app/admin/add-project/add-project.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-project',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProjectComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  projForm = this.fb.group({
+    title: [''],
+    type: [''],
+    description:[''],
+  });
+
+  async onSubmit() {
+    let formData = {
+      title: this.projForm.get('title').value,
+      type: this.projForm.get('type').value,
+      description: this.projForm.get('description').value,
+      observations: [],
+    };
+
+    try {
+      const res = await this.http
+        .post('http://localhost:3000/project/create', formData)
+        .toPromise();
+
+      this.router.navigateByUrl('projects');
+    } catch (err) {
+      window.alert(err.message);
+    }
+  }
 }
+
+
+
