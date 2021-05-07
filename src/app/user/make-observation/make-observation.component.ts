a7d2e8b15d6b527587d51669525c8b844892a593
jnew777 <newj@oregonstate.edu>
2021-05-06 22:19:34 -0400

    create observation form update

--- src/app/user/make-observation/make-observation.component.ts
+++ src/app/user/make-observation/make-observation.component.ts
@@ -5,80 +5,71 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { GeolocationService } from '@ng-web-apis/geolocation';
 
-
-
 @Component({
   selector: 'app-make-observation',
   templateUrl: './make-observation.component.html',
-  styleUrls: ['./make-observation.component.scss']
+  styleUrls: ['./make-observation.component.scss'],
 })
 export class MakeObservationComponent implements OnInit {
+  public latitude;
+  public longitude;
 
-    public latitude;
-    public longitude;    
+  user: any;
 
-    user: any;
+  constructor(
+    private usersService: UserService,
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+    private readonly geolocation$: GeolocationService
+  ) {}
 
-    constructor(
-        private usersService: UserService,
-        private fb: FormBuilder,
-        private http: HttpClient,
-        private router: Router,
-        private readonly geolocation$: GeolocationService
-        ) {}
+  obsForm = this.fb.group({
+    username: [''],
+    title: [''],
+    date: Date,
+    observation: [''],
+    city: [''],
+    state: [''],
+    longitude: Number,
+    latitude: Number,
+  });
 
-    obsForm = this.fb.group({
-        username: [''],
-        title: [''],
-        date: Date,
-        observation: [''],
-        city: [''],
-        state: [''],
-        longitude: Number,
-        latitude: Number
+  async ngOnInit() {
+    this.user = await this.usersService.getUserInfo();
+    console.log(this.user);
+  }
+
+  getLocation() {
+    this.geolocation$.subscribe((position) => {
+      this.longitude = position.coords.longitude;
+      this.latitude = position.coords.latitude;
     });
+  }
 
-    async ngOnInit() {
-        this.user = await this.usersService.getUserInfo();
-        console.log(this.user);
-    }
-
-    getLocation(){
-        this.geolocation$.subscribe(position => {
-            this.longitude = position.coords.longitude;
-            this.latitude = position.coords.latitude;
-        });
+  async onSubmit() {
+    let obsData = {
+      user: this.user.username,
+      title: this.obsForm.get('title').value,
+      text: this.obsForm.get('observation').value,
+      date: this.obsForm.get('date').value,
+      city: this.obsForm.get('city').value,
+      state: this.obsForm.get('state').value,
+      longitude: this.obsForm.get('longitude').value,
+      latitude: this.obsForm.get('latitude').value,
     };
+    try {
+      const res: any = await this.http
+        .post('http://localhost:3000/observation/create', obsData)
+        .toPromise();
 
-    async onSubmit() {
-        let obsData = {
-          user: this.user.username,
-          title: this.obsForm.get('title').value,
-          text: this.obsForm.get('observation').value,
-          date: this.obsForm.get('date').value,
-          city: this.obsForm.get('city').value,
-          state: this.obsForm.get('state').value,
-          longitude: this.obsForm.get('longitude').value,
-          latitude: this.obsForm.get('latitude').value
-        };
-        try {
-            const res: any = await this.http
-              .post('http://localhost:3000/observation/create', obsData)
-              .toPromise();
- 
-            var userObs = { observation: res.observation._id }
-            //console.log(userObs);
+      var userObs = { observation: res.observation._id };
+      //console.log(userObs);
 
-            console.log(res);
-            this.obsForm.reset();
-
-          } catch (err) {
-            window.alert(err.message);
-          }
+      console.log(res);
+      this.obsForm.reset();
+    } catch (err) {
+      window.alert(err.message);
     }
+  }
 }
-
-
-
-
-
