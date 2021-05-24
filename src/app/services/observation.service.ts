f2a4c9a21588b9d39beb5903c662a447b676200e
jnew777 <newj@oregonstate.edu>
2021-05-24 11:54:32 -0400

    Re-added get  one observation service

--- src/app/services/observation.service.ts
+++ src/app/services/observation.service.ts
@@ -14,6 +14,12 @@ export class ObservationService {
       .toPromise();
   }
 
+  getOneObservation(observationID): Promise<any> {
+    return this.http
+      .get(environment.backendUrl + 'observation/' + observationID)
+      .toPromise();
+  }
+
   getAllObservations(): Promise<any> {
     return this.http
       .get(environment.backendUrl + 'observation/observations')
