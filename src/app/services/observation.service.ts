042ac5ac48dec2c6c1eb072cae557e11655cc4eb
penninkn <morrikai@oregonstate.edu>
2021-05-24 19:51:21 -0400

    merge main

--- src/app/services/observation.service.ts
+++ src/app/services/observation.service.ts
@@ -6,8 +6,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class ObservationService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getObservation(observationID: any): any {
     throw new Error('Method not implemented.');
@@ -47,12 +46,6 @@ export class ObservationService {
   getUserObservations(projectId: string): Promise<any> {
     return this.http
       .get(environment.backendUrl + 'observation/user/' + projectId)
-      .toPromise();
-  }
-
-  getOneObservation(observationId): Promise<any> {
-    return this.http
-      .get(environment.backendUrl + 'observation/' + observationId)
       .toPromise();
   }
 }
